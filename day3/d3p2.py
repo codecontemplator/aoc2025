2aab87812376e0a14d429f158ef71d93afc85fda
Daniel Brännström <daniel@example.com>
2025-12-03 22:37:01 +0100

    d3p2

---
+++ day3/d3p2.py
@@ -0,0 +1,22 @@
+with open("input.txt", "r") as file:
+    rows = file.read().splitlines()
+
+def mknum(row, i, j, k):
+    return int(''.join([ ch for p, ch in enumerate(row) if p not in (i, j, k) ]))
+
+def process_row(rowin):
+    row = list(map(int, list(rowin)))
+    rowlen = len(row)
+    startpos = 0
+    result = []
+    for i in range(12):
+        endpos = rowlen - 11 + i
+        selected = max(row[startpos:endpos])
+        selected_index = row.index(selected, startpos, endpos)
+        startpos = selected_index + 1
+        result.append(selected)
+    return int(''.join(map(str,result)))
+
+print(sum(list(map(process_row, rows))))
+
+
