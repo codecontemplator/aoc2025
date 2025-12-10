adcaa06dac788cd6139bb6ef2da8f172563ffc9b
Daniel Brännström <daniel.brannstrom@walley.se>
2025-12-10 19:37:45 +0100

    simplified

--- day10/d10p1.py
+++ day10/d10p1.py
@@ -23,10 +23,7 @@ def parse_row(s):
     p3f = list(map(int,p3.split(",")))
     return (p1f, p2f, p3f)
 
-#goal, btns_orig, _ = parse_row("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}");
-#btns = list(map(lambda x: sum([ 2**i for i in x]), btns_orig))
-
-def search(goal, btns, lights, pressed, cnt, g_min_cnt):
+def search(goal, btns, lights, pressed, cnt):
     from collections import deque
     
     queue = deque([(lights, pressed, cnt)])
@@ -37,9 +34,6 @@ def search(goal, btns, lights, pressed, cnt, g_min_cnt):
         
         if lights == goal:
             return (cnt, pressed)
-        
-        if g_min_cnt is not None and cnt >= g_min_cnt:
-            continue
         
         for i, btn in enumerate(btns):
             if pressed[i] < 2:
@@ -54,19 +48,13 @@ def search(goal, btns, lights, pressed, cnt, g_min_cnt):
     return (None, None)
 
 
-# (cnt,pressed) = search(0, dict([(i,0) for i in range(len(btns))]), 0, None)
-# print(pressed)
-# for i,cnt in pressed.items():
-#     if cnt > 0:
-#         print(f"{btns_orig[i]} * {cnt}")
-
 parsed_rows = [ parse_row(row) for row in rows ]
 total_cnt = 0
 for goal, btns_orig, _ in parsed_rows:
     btns = [ sum([ 2**i for i in btn]) for btn in btns_orig]
     no_lights = 0
     no_pressed = dict([(i,0) for i in range(len(btns))])
-    cnt, _ = search(goal, btns, no_lights, no_pressed, 0, None)
+    cnt, _ = search(goal, btns, no_lights, no_pressed, 0)
     print(cnt)
     total_cnt += cnt
 
